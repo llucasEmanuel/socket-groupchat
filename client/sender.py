26500c57d27879c6034a992a3f015c2064f78ec5
CosmoLucs <cosmolucs@gmail.com>
2025-06-29 15:14:27 -0300

    feat(server): ensure consistent formatting and create main function

--- client/sender.py
+++ client/sender.py
@@ -1,8 +1,9 @@
+# client/sender.py
 import os
 from config.settings import SERVER_IP, SERVER_PORT, BUFFER_SIZE
 
 def send_file(sock, file_path):
-    sock.sendto(os.path.basename(file_path).encode(), (SERVER_IP, SERVER_PORT)) # N sei se isso funcina
+    sock.sendto(os.path.basename(file_path).encode(), (SERVER_IP, SERVER_PORT))
     with open(file_path, "rb") as f:
         while True:
             data = f.read(BUFFER_SIZE)
@@ -11,3 +12,12 @@ def send_file(sock, file_path):
             sock.sendto(data, (SERVER_IP, SERVER_PORT))
     sock.sendto(b'EOF', (SERVER_IP, SERVER_PORT))
     print(f"Arquivo '{file_path}' enviado ao servidor.")
+
+def receive_file(sock, output_name):
+    with open(output_name, "wb") as f:
+        while True:
+            data, _ = sock.recvfrom(BUFFER_SIZE)
+            if data == b'EOF':
+                break
+            f.write(data)
+    print(f"Arquivo devolvido salvo como '{output_name}'")
--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,3 @@
 SERVER_IP = 'localhost'
-SERVER_PORT = 12345
+SERVER_PORT = 12345 # Acho que isso é o padrão para UDP
 BUFFER_SIZE = 1024
