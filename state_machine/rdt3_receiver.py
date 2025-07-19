f44bef351d33616e0a677f2114e5a283c53080e9
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2025-07-19 18:49:11 -0300

    chore: correct transition dict comments

--- state_machine/rdt3_receiver.py
+++ state_machine/rdt3_receiver.py
@@ -14,15 +14,15 @@ class RDT3Receiver:
         self.__state = WAIT_PKT_0
         self.__transitions = {
             WAIT_PKT_0: {
-                # Packet 0 received at wait 0 state
+                # Packet 0 received at the wait 0 state
                 RCV_RIGHT_PKT_0: WAIT_PKT_1,
-                # Packet 1 or corrupt packet received at wait 0 state
+                # Packet 1 or corrupt packet received at the wait 0 state
                 RCV_WRONG_PKT_0: WAIT_PKT_0,
             },
             WAIT_PKT_1: {
-                # Packet 1 received at wait 1 state
+                # Packet 1 received at the wait 1 state
                 RCV_RIGHT_PKT_1: WAIT_PKT_0,
-                # Packet 0 or corrupt packet received at wait 1 state
+                # Packet 0 or corrupt packet received at the wait 1 state
                 RCV_WRONG_PKT_1: WAIT_PKT_1,
             }
         }
