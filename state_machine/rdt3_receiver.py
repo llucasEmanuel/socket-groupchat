83911732b56faf8f24c7001ed52f1893ec94f395
llucasEmanuel <lluukkaass62@gmail.com>
2025-07-19 18:44:23 -0300

    feat: add rdt3.0 receiver state machine logic

---
+++ state_machine/rdt3_receiver.py
@@ -0,0 +1,44 @@
+# RDT3.0 Receiver states
+WAIT_PKT_0 = "WAIT_PKT_0"
+WAIT_PKT_1 = "WAIT_PKT_1"
+
+# Transitions available
+RCV_RIGHT_PKT_0 = "RCV_RIGHT_PKT_0"
+RCV_RIGHT_PKT_1 = "RCV_RIGHT_PKT_1"
+RCV_WRONG_PKT_0 = "RCV_WRONG_PKT_0"
+RCV_WRONG_PKT_1 = "RCV_WRONG_PKT_1"
+
+class RDT3Receiver:
+    def __init__(self):
+        # initial_state
+        self.__state = WAIT_PKT_0
+        self.__transitions = {
+            WAIT_PKT_0: {
+                # Packet 0 received at wait 0 state
+                RCV_RIGHT_PKT_0: WAIT_PKT_1,
+                # Packet 1 or corrupt packet received at wait 0 state
+                RCV_WRONG_PKT_0: WAIT_PKT_0,
+            },
+            WAIT_PKT_1: {
+                # Packet 1 received at wait 0 state
+                RCV_RIGHT_PKT_1: WAIT_PKT_0,
+                # Packet 0 or corrupt packet received at wait 0 state
+                RCV_WRONG_PKT_1: WAIT_PKT_1,
+            }
+        }
+
+    def transition(self, event):
+        state_transitions = self.__transitions[self.__state]
+        try:
+            new_state = state_transitions[event]
+
+            print(f"({self.__state}) -> [{event}] -> ({new_state})")
+            self.__state = new_state
+
+        except KeyError:
+            raise KeyError(f"Invalid transition: event '{event}' from state '{self.__state}'")
+
+    def get_state(self):
+        return self.__state
+        
+    
