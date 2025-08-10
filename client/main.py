8c221bcbecaee60dabab25eb931daf5dcb369fdd
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2025-08-10 11:17:00 -0300

    chore: delete \ola prototype

--- client/main.py
+++ client/main.py
@@ -3,22 +3,6 @@ from client.client import client_receive_file, client_send_file
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    while (True):
-        # Lê o comando do usuário
-        string = input("> ")
-
-        # Separa as substrings por espaço
-        tokens = string.split()
-
-        if tokens[0] == "/ola":
-            if len(tokens) == 1:
-                print("Nome de usuário não fornecido.")
-                continue
-            # Considera que o nome do usuário não tem espaço
-            username = tokens[1]
-            
-            # Envia uma mensagem do tipo /ola para o servidor com o username
 
     # Mude para o nome do arquivo que quer enviar para o servidor (deve estar na pasta client/data/)
     file_name = "ah_eh.jpg"
