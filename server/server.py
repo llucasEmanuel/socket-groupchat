0d8e035f3386b0cb40dad49a42340eafb1dd5eb1
Gabriel Alves <136081929+Gagdm@users.noreply.github.com>
2025-08-10 22:37:27 -0300

    feat: add message broadcasting and client listing (#9)
    * feat: add rdt3.0 base class
    
    * feat: add rdt3.0 receiver state machine logic
    
    * Delete rdt directory
    
    * chore: correct transition dic commentary
    
    * chore: correct transition dict comments
    
    * feat: add sender state machine
    
    * chore: add state machine test file
    
    * feat: add timer class
    
    * feat: begin implementing state machine
    
    * refactor: change transition method parameter from event to state (#4)
    
    Co-authored-by: lessl <lessl@hwc29.cin.ufpe.br>
    
    * Update README.md
    
    * feat: add rdt3.0 receiver
    
    * feat: add rdt3.0 sender
    
    * fix: small import fix
    
    * feat: add packet loss
    
    * fix: small print error
    
    * fix: add packet loss to ack messages
    
    * refactor: add comments and prints
    
    * feat: add \ola command
    
    * feat: add Client, ClientRegister and Server classes
    
    * chore: delete \ola prototype
    
    * add /list response from server
    
    * fix: change client list message to unique string
    
    * feat: add client_list methods
    
    * feat: add client list handling on Client
    
    ---------
    
    Co-authored-by: llucasEmanuel <lluukkaass62@gmail.com>
    Co-authored-by: Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
    Co-authored-by: lessl <lessl@hwc24.cin.ufpe.br>
    Co-authored-by: lessl <lessl@g4c02.cin.ufpe.br>
    Co-authored-by: rama39 <rama@cin.ufpe.br>
    Co-authored-by: lessl <lessl@hwc29.cin.ufpe.br>
    Co-authored-by: lessl <lessl@g1c44.cin.ufpe.br>

--- server/server.py
+++ server/server.py
@@ -1,5 +1,78 @@
 import os
-from utils.utils import receive_file, send_file
+from utils.utils import receive_file, send_file, send_message
+
+class ClientRegister:
+    def __init__(self, username, addr):
+        self.username = username
+        # tupla: (ip, porta)
+        self.addr = addr
+
+class Server: 
+    def __init__(self):
+        # cria uma lista vazia de ClientRegister
+        self.client_list = [ClientRegister]
+        self.ban_list = [ClientRegister]
+        
+    def add_client(self, sock, username, addr):   
+
+        # caso esteja banido
+        for banned_client in self.ban_list:
+            if(username == banned_client.username):
+
+                output_message = f"\033[31m[Server] ☠️ Acesso negado ☠️: '{username}' foi banido do chat!! \033[0m"
+                send_message(sock, addr, output_message)
+                print(output_message)
+                return
+            
+        # caso já exista um username igual no chat
+        for client in self.client_list:
+            if(username == client.username):
+
+                output_message = f"\033[33m[Server] ⚠️ o username '{username}' já está em uso. ⚠️\033[0m"
+                send_message(sock, addr, output_message)
+                print(output_message)
+                return
+
+        # adicionando novo cliente
+        client = ClientRegister(username, addr)
+        self.client_list.append(client)  
+
+        output_message = f"[Server] '{username}' foi adicionado ao chat."
+        self.broadcast_message(sock, output_message)
+        print(output_message)
+
+    def remove_client(self, sock, username, ban_on=False):
+        for client in self.client_list:
+
+            if(username == client.username):
+                self.client_list.remove(client)
+
+                if(not ban_on):
+                    output_message = f"[Server] '{username}' saiu do chat."
+                    print(output_message)
+                    self.broadcast_message(sock, output_message)
+                else:
+                    print(f"[Server] '{username}' foi removido do chat.")
+
+                return
+
+        output_message = "\033[33m[Server] ⚠️ Ocorreu um erro, não foi possível remover o usuário. ⚠️\033[0m"
+        print(output_message)
+                
+    # Envia a lista de clientes para o cliente de endereço addr que usou /list
+    def send_client_list(self, sock, addr):
+        client_names = [client.username for client in self.client_list]
+
+        # Concatena os nomes dos clientes em uma string (usa \0 para separar cada username)
+        all_names_string = "\0".join(client_names)
+
+        # Envia apenas a string para o cliente
+        send_message(sock, addr, all_names_string)
+
+    def broadcast_message(self, sock, message):
+
+        for client in self.client_list:
+            send_message(sock, client.addr, message)
 
 class ClientRegister:
     def __init__(self, username, addr):
